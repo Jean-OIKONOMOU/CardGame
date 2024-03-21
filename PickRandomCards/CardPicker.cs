3edc8e18a8545d96f1349f940ed21ff84383007c
jean oikonomou <jean.oikonomou@gmail.com>
2024-03-21 18:23:12 +0100

    looks good, time to implement this in a wpf project

--- PickRandomCards/CardPicker.cs
+++ PickRandomCards/CardPicker.cs
@@ -9,17 +9,15 @@ namespace PickRandomCards
 {
     internal class CardPicker
     {
-        public static List<string> PickSomeCards(int cards)
+        public static string[] PickSomeCards(int numberOfCards)
         {
-            //string[] randomCards = new string[] { };
-            var randomCards = new List<string>() { };
-            for (int i = 0; i < cards; i++)
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
             {
-                string card = NewUniqueCard(RandomValue() + " " + RandomSuit(), randomCards);
-                randomCards.Add(card);
+                pickedCards[i] = NewUniqueCard(RandomValue() + " " + RandomSuit(), pickedCards);
             }
 
-            return randomCards;
+            return pickedCards;
         }
 
         static Random Random = new Random();
@@ -28,19 +26,15 @@ namespace PickRandomCards
             int rand = Random.Next(1, 13);
             string[] cardValues = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven",
                                    "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
-            //int rand = Random.Next(0, 3);
-            //string[] cardValues = {"Ace", "Two", "Three"};
             return cardValues[rand];
         }
         static string RandomSuit() {
             int rand = Random.Next(1, 4);
             string[] cardSuits = { "of Spades", "of Clubs", "of Hearts", "of Diamonds" };
-            //int rand = Random.Next(0, 2);
-            //string[] cardSuits = {"of Spades", "of Clubs"};
             return cardSuits[rand];
         }   
 
-        static string NewUniqueCard(string card, List<string> cards)
+        static string NewUniqueCard(string card, string[] cards)
         {
             if (cards.Contains(card)) {
                 return NewUniqueCard(RandomValue() + " " + RandomSuit(), cards);
