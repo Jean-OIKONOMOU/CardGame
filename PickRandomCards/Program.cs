674028cf36b1c598b9376d5cf3896de53c016def
jean oikonomou <jean.oikonomou@gmail.com>
2024-03-21 18:41:34 +0100

    Added a more intuitive way to ask for cards as well as a way to repeat the program if so desired.

--- PickRandomCards/Program.cs
+++ PickRandomCards/Program.cs
@@ -1,20 +1,42 @@
 ﻿using PickRandomCards;
 
-Console.WriteLine("Hello master, how many random cards would you like ?");
+static void AskForADeck()
+{ 
+    string? line = Console.ReadLine();
 
-string? line = Console.ReadLine();
-
-if (line == null)
-{
-    line = "1";
-}
-
-if (int.TryParse(line, out int id))
-{
-    string[] deck = CardPicker.PickSomeCards(id);
-    foreach (var card in deck)
+    if (line == null)
     {
-        Console.WriteLine($"==> {card}");
+        line = "1";
     }
-    Console.WriteLine($"Here is your {deck.Length} card deck, master.");
-}
+
+    if (int.TryParse(line, out int id)) {
+        string[] deck = CardPicker.PickSomeCards(id);
+        foreach (var card in deck)
+        {
+            Console.WriteLine($"==> {card}");
+        }
+        Console.WriteLine($"Here is your {deck.Length} card deck, master.");
+        Console.WriteLine("Would you like to ask again, master ? Type Y or y to try again, anything else will end the program.");
+        string? tryAgain = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(tryAgain) && !string.IsNullOrWhiteSpace(tryAgain))
+        {
+            if (tryAgain == "Y" || tryAgain == "y") {
+                Console.WriteLine("Master, please, provide me with a digit.");
+                AskForADeck();
+            }
+            else
+            {
+                Console.WriteLine("Goodbye, master.");
+                return;
+            }
+        }
+
+    } else {
+        Console.WriteLine($"Master, I could not interpret your digit. Please, try again.");
+        AskForADeck();
+    }
+};
+
+Console.WriteLine("Hello master, how many random cards would you like ? Please, provide me with a digit.");
+AskForADeck();
+
